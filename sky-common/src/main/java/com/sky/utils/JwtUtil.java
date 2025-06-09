7caea5fa51f0dc8bbf023f5ab9c1406be826655d
millionfire <millionfire123@gmail.com>
2025-06-09 14:56:24 +0800

    feat：适配到jdk21，升级springboot3.x.x配置

--- sky-common/src/main/java/com/sky/utils/JwtUtil.java
+++ sky-common/src/main/java/com/sky/utils/JwtUtil.java
@@ -1,12 +1,12 @@
 package com.sky.utils;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.Map;
+import javax.crypto.SecretKey;
 
 public class JwtUtil {
     /**
@@ -19,23 +19,22 @@ public class JwtUtil {
      * @return
      */
     public static String createJWT(String secretKey, long ttlMillis, Map<String, Object> claims) {
-        // 指定签名的时候使用的签名算法，也就是header那部分
-        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
-
         // 生成JWT的时间
         long expMillis = System.currentTimeMillis() + ttlMillis;
         Date exp = new Date(expMillis);
 
+        // 创建密钥
+        SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
         // 设置jwt的body
-        JwtBuilder builder = Jwts.builder()
+        return Jwts.builder()
                 // 如果有私有声明，一定要先设置这个自己创建的私有的声明，这个是给builder的claim赋值，一旦写在标准的声明赋值之后，就是覆盖了那些标准的声明的
                 .setClaims(claims)
                 // 设置签名使用的签名算法和签名使用的秘钥
-                .signWith(signatureAlgorithm, secretKey.getBytes(StandardCharsets.UTF_8))
+                .signWith(key)
                 // 设置过期时间
-                .setExpiration(exp);
-
-        return builder.compact();
+                .setExpiration(exp)
+                .compact();
     }
 
     /**
@@ -46,13 +45,16 @@ public class JwtUtil {
      * @return
      */
     public static Claims parseJWT(String secretKey, String token) {
-        // 得到DefaultJwtParser
-        Claims claims = Jwts.parser()
+        // 创建密钥
+        SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+        
+        // 解析token
+        Claims claims = Jwts.parserBuilder()
                 // 设置签名的秘钥
-                .setSigningKey(secretKey.getBytes(StandardCharsets.UTF_8))
+                .setSigningKey(key)
+                .build()
                 // 设置需要解析的jwt
                 .parseClaimsJws(token).getBody();
         return claims;
     }
-
 }

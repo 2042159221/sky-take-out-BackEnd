dcb5ead91014c54d8c88899a274a78bbe1d624ca
millionfire <millionfire123@gmail.com>
2025-06-09 15:56:18 +0800

    feat：优化全局异常处理器

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,13 +1,16 @@
 package com.sky.handler;
 
-import com.sky.exception.BaseException;
+import com.sky.exception.AccountLockedException;
+import com.sky.exception.AccountNotFoundException;
+import com.sky.exception.PasswordErrorException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
- * 全局异常处理器，处理项目中抛出的业务异常
+ * 全局异常处理器
  */
 @RestControllerAdvice
 @Slf4j
@@ -19,9 +22,52 @@ public class GlobalExceptionHandler {
      * @return
      */
     @ExceptionHandler
-    public Result exceptionHandler(BaseException ex){
+    public Result exceptionHandler(AccountLockedException ex){
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
     }
 
+    /**
+     * 捕获业务异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(AccountNotFoundException ex){
+        log.error("异常信息：{}", ex.getMessage());
+        return Result.error(ex.getMessage());
+    }
+
+    /**
+     * 捕获业务异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(PasswordErrorException ex){
+        log.error("异常信息：{}", ex.getMessage());
+        return Result.error(ex.getMessage());
+    }
+
+    /**
+     * 处理HTTP请求方法不支持的异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(HttpRequestMethodNotSupportedException ex){
+        log.error("异常信息：{}", ex.getMessage());
+        return Result.error("请求方法不支持，请使用正确的HTTP方法");
+    }
+
+    /**
+     * 处理未知异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(Exception ex){
+        log.error("异常信息：{}", ex.getMessage());
+        return Result.error("系统繁忙，请稍后再试");
+    }
 }

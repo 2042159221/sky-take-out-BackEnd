d39f5aadeabf1518ed1b7c7bc072157513da2af5
millionfire <millionfire123@gmail.com>
2025-06-23 15:16:52 +0800

    fix;优化lombok版本配置

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+/**
+ * @author millionfire
+ */
 @SpringBootApplication
 @EnableTransactionManagement //开启注解方式的事务管理
 @Slf4j
